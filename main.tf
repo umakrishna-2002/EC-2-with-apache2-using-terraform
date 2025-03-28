7058603af3d3f5ef4d26622f60070c47bfb5efc8
umakrishna-2002 <umakrishnamarineni@gmail.com>
2025-03-28 15:26:26 +0530

    modified file

--- main.tf
+++ main.tf
@@ -3,10 +3,39 @@ provider "aws" {
     profile = "Nani"
 }
 
+resource "aws_security_group" "web_server_sg" {
+  name        = "web-server-sg"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Replace with your IP for better security
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web-server" {
   ami           = "ami-071226ecf16aa7d96"
   instance_type = "t2.micro"
   key_name      = "k8s"
+  security_groups = [aws_security_group.web_server_sg.name]
   user_data     = <<-EOF
             #!/bin/bash
             sudo apt update -y
